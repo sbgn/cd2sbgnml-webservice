480b0068bc8f0bdba951b64eb82c9975533e023c
Gökberk Karaca <gskaraca1905@gmail.com>
2018-03-13 20:35:12 +0300

    Add cd2sbgnml library as a dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
+import fr.curie.cd2sbgnml.Cd2SbgnmlScript;
+import fr.curie.cd2sbgnml.Sbgnml2CdScript;
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
+        Sbgnml2CdScript sbgnml2CdScript = new Sbgnml2CdScript();
+        Cd2SbgnmlScript cd2SbgnmlScript = new Cd2SbgnmlScript();
+
         get("/hello", (reg, res) -> "Hello World");
     }
 }
