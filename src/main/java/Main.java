f249e7d5a08e00b2860834345526c4933b542db1
Gökberk Karaca <gskaraca1905@gmail.com>
2018-03-15 11:26:38 +0300

    Change the jar file of cd2sbgnml library with the one including dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import fr.curie.cd2sbgnml.Cd2SbgnmlScript;
 import fr.curie.cd2sbgnml.Sbgnml2CdScript;
 
-import static spark.Spark.*;
-
 public class Main {
     public static void main(String[] args) {
-        Cd2SbgnmlScript.convert("./samples/anchorPoints.xml", "./samples/anchorPoints.sbgn");
+        Cd2SbgnmlScript.convert("samples/anchorPoints.xml", "samples/anchorPoints.sbgn");
         Sbgnml2CdScript.convert("samples/anchorPoints.sbgn", "samples/anchorPointsConverted.xml");
-
-        get("/hello", (reg, res) -> "Hello World");
     }
 }
