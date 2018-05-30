ce3f65e4fe0d3559aff5265cfb9454a784f2c706
Gökberk Karaca <gskaraca1905@gmail.com>
2018-05-30 12:20:11 +0300

    Change port number to 8080

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         
+        port(8080);
         options("/*",
                 (request, response) -> {
 
